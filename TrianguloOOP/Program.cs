3cf9d2942340b72ff4291c57f5292eb19003f593
Rafael Santos <rafaelsantos138@hotmail.com>
2023-04-05 18:19:20 -0300

    Add project files.

---
+++ TrianguloOOP/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TrianguloOOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new();
+
+            Console.WriteLine("------ Triangulo Orientado à Objetos ------\n");
+
+            Console.WriteLine(">> Insira os lados do triângulo.\n>> A medida de um dos lados deve ser menor que a soma dos outros dois lados");
+
+            triangulo.ObterLados();
+
+            if (triangulo.ValidarTriangulo() == true)
+                Environment.Exit(0);
+
+            triangulo.EncontrarTipoTriangulo();
+            
+        }
+    }
+}
---
+++ TrianguloOOP/Triangulo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text;
+
+namespace TrianguloOOP
+{
+    internal class Triangulo
+    {
+
+        public int LadoX { get; set; }
+        public int LadoY { get; set; }
+        public int LadoZ { get; set; }
+
+        public void ObterLados()
+        {
+            bool continuar = true;
+
+            Console.Write("\n>> Insira o primeiro lado: ");
+
+            while (continuar == true)
+            {
+                try
+                {
+                    LadoX = Convert.ToInt32(Console.ReadLine());
+                    continuar = false;
+                }
+                catch(FormatException) 
+                {
+                    Console.Write("\n>> Formato inválido, tente novamente: ");
+                    continuar = true;
+                }               
+            }
+
+            continuar = true;
+
+            Console.Write("\n>> Insira o segundo lado: ");
+
+            while (continuar == true)
+            {
+                try
+                {
+                    LadoY = Convert.ToInt32(Console.ReadLine());
+                    continuar = false;
+                }
+                catch (FormatException)
+                {
+                    Console.Write("\n>> Formato inválido, tente novamente: ");
+                    continuar = true;
+                }
+            }
+
+            continuar = true;
+
+            Console.Write("\n>> Insira o terceiro lado: ");
+
+            while (continuar == true)
+            {
+                try
+                {
+                    LadoZ = Convert.ToInt32(Console.ReadLine());
+                    continuar = false;
+                }
+                catch (FormatException)
+                {
+                    Console.Write("\n>> Formato inválido, tente novamente: ");
+                    continuar = true;
+                }
+            }
+        }
+        public bool ValidarTriangulo()
+        {
+            bool ehValido = false;
+            if (LadoX >= LadoY + LadoZ)
+            {
+                Console.WriteLine(">> Triângulo Inválido.");
+                ehValido = true;
+            }
+
+            else if (LadoY >= LadoX + LadoZ)
+            {
+                Console.WriteLine(">> Triângulo Inválido.");
+                ehValido = true;
+            }
+
+            else if (LadoZ >= LadoX + LadoY)
+            {
+                Console.WriteLine(">> Triângulo Inválido.");
+                ehValido = true;
+            }
+
+            return ehValido;
+        }
+        public void EncontrarTipoTriangulo()
+        {
+            if (LadoX != LadoY && LadoX != LadoZ && LadoY != LadoZ)
+                Console.WriteLine(">> Triângulo escaleno");
+            else if (LadoX == LadoY && LadoX == LadoZ && LadoY == LadoZ)
+                Console.WriteLine(">> Triângulo equilátero");
+            else if (LadoX == LadoY || LadoX == LadoZ || LadoY == LadoZ)
+                Console.WriteLine(">> Triângulo isóceles");
+            else
+                Console.WriteLine(">> Triângulo Inválido");
+        }
+    }
+}
